cf2fc0b4daf39bb95077aa4a45648c030863ca1b
Jhonnatan Vazquez <jhonykingmusic@hotmail.com>
2024-11-01 00:56:24 -0500

    application finished

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,10 +4,27 @@ function twitter() {
   const btnToSend = document.getElementById("btnSend");
   const newTweet = btnToSend.addEventListener("click", sendTweet);
   document.getElementById("tweet-list").addEventListener("click", deleteTweet);
+  document.addEventListener("DOMContentLoaded", printTweets());
+
+  function printTweets() {
+    let tweetsLS = loadingTweetsLS();
+    tweetsLS.forEach(function (tweet) {
+      const li = document.createElement("li");
+      li.textContent = tweet;
+
+      //Create the anchor and appendding to the li
+      const a = document.createElement("a");
+      a.textContent = "X";
+      a.classList.add("remove-tweet");
+      li.appendChild(a);
+      //Append the li to the tweet list
+      document.getElementById("tweet-list").appendChild(li);
+    });
+  }
 
   function loadingTweetsLS() {
     let tweets;
-    const tweetsLS = localStorage.getItem("tweetsLS");
+    const tweetsLS = localStorage.getItem("tweets");
     if (tweetsLS === null) {
       tweets = [];
     } else {
@@ -37,20 +54,32 @@ function twitter() {
     //Reseting the textarea
     document.getElementById("tweet").value = "";
 
-    addTweetLS();
+    addTweetLS(newTweet);
   }
 
   function addTweetLS(newTweet) {
     let tweets = loadingTweetsLS();
     tweets.push(newTweet);
     console.log(tweets);
-    localStorage.setItem(JSON.stringify(tweets));
+    localStorage.setItem("tweets", JSON.stringify(tweets));
   }
 
   function deleteTweet(e) {
     if (e.target.classList.contains("remove-tweet")) {
+      deleteTweetfromLS(e.target.parentElement.textContent);
       e.target.parentElement.remove();
     }
+  }
+
+  function deleteTweetfromLS(tweet) {
+    tweet = tweet.substring(0, tweet.length - 1);
+    let tweets = loadingTweetsLS();
+    tweets.forEach(function (tweetLS, index) {
+      if (tweet === tweetLS) {
+        tweets.splice(index, 1);
+      }
+    });
+    localStorage.setItem("tweets", JSON.stringify(tweets));
   }
 }
 
